ae29ccb336bd85d8b3613589abb8522efd5e0c97
Dawn Carroll <dsc254@gmail.com>
2018-01-24 11:28:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ function updateObjectWithKeyAndValue(obj, key, value) {
   return Object.assign({}, obj, { [key]: value });
 
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+  object.key = value;
+  return object;
+  
+}
