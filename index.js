04b0896689357e99b428a2204817d21bceb8a6c5
Dawn Carroll <dsc254@gmail.com>
2018-01-24 11:10:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ var recipes = {
   
 };
 
-function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = "value";
-  return object;
-}
+
 
 function updateObjectWithKeyAndValue(object, key, value) {
   
